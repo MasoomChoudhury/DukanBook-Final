eacfab0d6fdb5ea5cb951e63614665d1b1f8a11f
Masoom Choudhury <116518984+MasoomChoudhury@users.noreply.github.com>
2025-11-11 01:32:14 +0530

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive information.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,28 +1,25 @@
-// Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: "billing-software-74d41.firebaseapp.com",
-  projectId: "billing-software-74d41",
-  storageBucket: "billing-software-74d41.appspot.com",
-  messagingSenderId: "619342921179",
-  appId: "1:619342921179:web:df26759b66a056f0bdfbfe",
-  measurementId: "G-WG3LJ0XD99"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+    if (firebaseConfig.apiKey && firebaseConfig.projectId) {
+        firebase.initializeApp(firebaseConfig);
+    } else {
+        console.error("Firebase configuration environment variables are not set. The app will not function correctly.");
+    }
 }
 
-// Initialize Cloud Firestore and get a reference to the service
 export const db = firebase.firestore();
 
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = firebase.auth();
